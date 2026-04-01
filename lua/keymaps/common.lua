1ba472e5d1f9d074d481023ba244ef9453c7a64b
saeegonee <saeegonee@yandex.ru>
2026-04-01 17:48:00 +0300

    gr template

--- lua/keymaps/common.lua
+++ lua/keymaps/common.lua
@@ -1,7 +1,8 @@
 vim.keymap.set("n", "<leader>w", "<Cmd>Explore<CR>")
 
 local opts = { noremap = true, silent = true }
-vim.keymap.set("n", "gd",           "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+vim.keymap.set("n", "gd",           "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
+vim.keymap.set("n", "gr",           "", opts)
 vim.keymap.set("n", "<Leader>fo",   ":lua vim.lsp.buf.format()<CR>", opts)
 
 -- move line
