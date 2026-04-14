25b4f4d947204095ed863bb58f2ad1a98b40b8c3
saeegonee <saeegonee@yandex.ru>
2026-04-14 09:13:16 +0300

    bufferline keymaps + witch keys

--- lua/keymaps/bufferline.lua
+++ lua/keymaps/bufferline.lua
@@ -1,3 +1,5 @@
 vim.keymap.set("n", "bn",    "<Cmd>BufferLineCycleNext<CR>")
 vim.keymap.set("n", "bm",    "<Cmd>BufferLineCyclePrev<CR>")
+vim.keymap.set("n", "bc",    "<Cmd>BufferLineCloseOthers<CR>")
+vim.keymap.set("n", "bd",    "<Cmd>bdelete<CR>")
 
