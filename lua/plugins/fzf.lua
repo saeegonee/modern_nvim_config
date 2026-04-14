5f903874a33695950f9908ecdbb323bde08b887e
saeegonee <saeegonee@yandex.ru>
2026-04-14 09:24:56 +0300

    keymaps

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -1,4 +1,4 @@
-local actions = require('fzf-lua.actions')
+-- local actions = require('fzf-lua.actions')
 require("fzf-lua").setup({
     winopts = {
         backdrop = 85,
@@ -18,26 +18,26 @@ require("fzf-lua").setup({
             "Themes/*"
         }
     },
-    keymap = {
-        builtin = {
-            ["<C-f>"] = "preview-page-down",
-            ["<C-b>"] = "preview-page-up",
-            ["<C-p>"] = "toggle-preview",
-        },
-        fzf = {
-            ["ctrl-a"] = "toggle-all",
-            ["ctrl-t"] = "first",
-            ["ctrl-g"] = "last",
-            ["ctrl-d"] = "half-page-down",
-            ["ctrl-u"] = "half-page-up",
-        }
-    },
-    actions = {
-        files = {
-            ["ctrl-q"] = actions.file_sel_to_qf,
-            ["ctrl-n"] = actions.toggle_ignore,
-            ["ctrl-h"] = actions.toggle_hidden,
-            ["enter"]  = actions.file_edit_or_qf,
-        }
-    }
+    -- keymap = {
+    --     builtin = {
+    --         ["<C-f>"] = "preview-page-down",
+    --         ["<C-b>"] = "preview-page-up",
+    --         ["<C-p>"] = "toggle-preview",
+    --     },
+    --     fzf = {
+    --         ["ctrl-a"] = "toggle-all",
+    --         ["ctrl-t"] = "first",
+    --         ["ctrl-g"] = "last",
+    --         ["ctrl-d"] = "half-page-down",
+    --         ["ctrl-u"] = "half-page-up",
+    --     }
+    -- },
+    -- actions = {
+    --     files = {
+    --         ["ctrl-q"] = actions.file_sel_to_qf,
+    --         ["ctrl-n"] = actions.toggle_ignore,
+    --         ["ctrl-h"] = actions.toggle_hidden,
+    --         ["enter"]  = actions.file_edit_or_qf,
+    --     }
+    -- }
 })
