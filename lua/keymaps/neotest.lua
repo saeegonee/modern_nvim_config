f52e143d6a56866715f4eeb9b9a91449b119281d
saeegonee <saeegonee@yandex.ru>
2026-04-15 11:57:56 +0300

    remove neotest

--- lua/keymaps/neotest.lua
+++
@@ -1,4 +0,0 @@
-vim.keymap.set("n", "<F6>",         "<Cmd>Neotest summary<CR>")
-vim.keymap.set("n", "<Leader>tr",   "<Cmd>Neotest run<CR>")
-vim.keymap.set("n", "<Leader>ts",   "<Cmd>Neotest stop<CR>")
-vim.keymap.set("n", "<Leader>to",   "<Cmd>Neotest output<CR>")
