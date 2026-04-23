95249bbb014cd01ef2a784327728c6266a9acc8b
saeegonee <saeegonee@yandex.ru>
2026-04-23 09:46:49 +0300

    refactor copy autocmd

--- lua/autocmd/copy.lua
+++ lua/autocmd/copy.lua
@@ -1,11 +1,10 @@
-local ag = vim.api.nvim_create_augroup
-
-local highlight_group = ag('YankHighlight', { clear = true })
-
-vim.api.nvim_create_autocmd('TextYankPost', {
-    pattern = '*',
-    callback = function()
-        vim.highlight.on_yank({ timeout = 170 })
-    end,
-    group = highlight_group,
-})
+vim.api.nvim_create_autocmd(
+    { "TextYankPost" },
+    {
+        group = vim.api.nvim_create_augroup("YankHighlight", { clear = true }),
+        pattern = "*",
+        callback = function()
+            vim.highlight.on_yank({ timeout = 170 })
+        end,
+    }
+)
