f52bcd602d26be7057b6bcbe2c316fb4c2acecd8
saeegonee <saeegonee@yandex.ru>
2026-07-22 10:07:49 +0300

    pylint

--- lua/plugins/py_lint.lua
+++ lua/plugins/py_lint.lua
@@ -8,10 +8,14 @@ lint.linters_by_ft = {
 local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
 -- Автокоманды для запуска линтинга при открытии буфера
-vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
+vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile", "BufWritePost" }, {
     group = lint_augroup,
     callback = function()
         lint.try_lint()
     end,
     desc = "Запустить линтинг при открытии файла или создании нового"
 })
+
+-- Set pylint to work in virtualenv
+lint.linters.pylint.cmd = 'python'
+lint.linters.pylint.args = {'-m', 'pylint', '-f', 'json'}
